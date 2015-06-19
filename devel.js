82f9b641b5c493528a911aaeb2b4b7f0deca6c04
Herbert Vojčík <herby@mailbox.sk>
2015-06-19 22:43:37 +0200

    files supporting development

---
+++ devel.js
@@ -0,0 +1,9 @@
+define([
+    './testing',
+    'amber/devel',
+    // --- packages used only during development begin here ---
+    'amber-attic/IDE'
+    // --- packages used only during development end here ---
+], function (amber) {
+    return amber;
+});
